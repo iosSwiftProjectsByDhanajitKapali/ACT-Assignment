7588fba5e76957cdaf41a17072388cb970998481
Dhanajit kapali <37000235+dhanajitkapali@users.noreply.github.com>
2021-06-15 09:39:08 +0530

    added button to go back to login page, added checks for all textfields, added function to dismiss keyboard, added access specifiers.

--- ACT-Assignment/ViewControllers/SignUpViewController.swift
+++ ACT-Assignment/ViewControllers/SignUpViewController.swift
@@ -9,16 +9,16 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
-    @IBOutlet var backgroundView: UIView!
-    @IBOutlet var nameView: UIView!
-    @IBOutlet var mobileNumberView: UIView!
-    @IBOutlet var emailView: UIView!
-    @IBOutlet var passwordView: UIView!
-    @IBOutlet var nameTextField: UITextField!
-    @IBOutlet var mobileTextField: UITextField!
-    @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
-    @IBOutlet var signupButton: UIButton!
+    @IBOutlet weak private var backgroundView: UIView!
+    @IBOutlet weak private var nameView: UIView!
+    @IBOutlet weak private var mobileNumberView: UIView!
+    @IBOutlet weak private var emailView: UIView!
+    @IBOutlet weak private var passwordView: UIView!
+    @IBOutlet weak private var nameTextField: UITextField!
+    @IBOutlet weak private var mobileTextField: UITextField!
+    @IBOutlet weak private var emailTextField: UITextField!
+    @IBOutlet weak private var passwordTextField: UITextField!
+    @IBOutlet weak private var signupButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +30,25 @@ class SignUpViewController: UIViewController {
         passwordView.layer.cornerRadius = 20.0
         signupButton.layer.cornerRadius = 20.0
         
+        //to hide keyboard
+        self.hideKeyboardWhenTappedAround()
     }
     
     @IBAction func signupButtonPressed(_ sender: UIButton) {
+        if let name = nameTextField.text, !name.isEmpty, let mobileNo = mobileTextField.text, !mobileNo.isEmpty, let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty {
+        
+    
+        }else{
+            nameTextField.placeholder = K.TextMessage.emptyNameMessage
+            mobileTextField.placeholder = K.TextMessage.emptyMobileNoMessage
+            emailTextField.placeholder = K.TextMessage.emptyEmailMessage
+            passwordTextField.placeholder = K.TextMessage.emptyPasswordMessage
+        }
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        //jump back to Login scene
+        navigationController?.popViewController(animated: true)
     }
     
 }
